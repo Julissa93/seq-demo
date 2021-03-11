407b4c125d87688b5d4e9c5db58f3b05ef680290
julissa93 <jnapoletano28@gmail.com>
2021-03-11 11:56:01 -0500

    adds notes

--- src/db.js
+++ src/db.js
@@ -1,8 +1,7 @@
-//const { userInfo } = require('node:os')
 const Sequelize = require('sequelize')
 
-const db = new Sequelize("postgres://localhost:5432/sequelize-demo", {logging: false})
-
+const db = new Sequelize("postgres://localhost:5432/sequelize-demo")
+//const db = new Sequelize("postgres://localhost:5432/sequelize-demo", {logging: false}) <-- this will remove logging of sql queries
 const User = db.define("user", {
     name: Sequelize.STRING,
     age: Sequelize.INTEGER,
@@ -20,6 +19,7 @@ Pet.belongsTo(User) //creates the foreign key column
 
 //console.log('Database object: ', db)
 
+//hooks - Check out this https://gist.github.com/Julissa93/6a6d29874d34a801d603d2522645025f to see what other Hooks you can use.
 User.beforeValidate(() => console.log('in before validate hook!!!'))
 User.afterValidate(() => console.log('in after validate!!!'))
 User.beforeCreate(() => console.log('in before create hook!!!'))
@@ -28,18 +28,31 @@ User.afterCreate(() => console.log('in after create hook!!!') )
 const connect = async () => {
     await db.sync({force: true}) //will sync models to the database instance that we created with sequelize
     const kevin = await User.create({
-        name: "Kevin",
+        name: 'Kevin',
         age: 26,
-        email: "kevin@kevin.com"
+        email: 'kevin@kevin.com'
     })
-    /*await User.bulkCreate([])*/
-    //console.log('magic methods: ', kevin.__proto__)
+    //await User.bulkCreate([]) <-- to create multiple instances - bulkCreate takes an array of objects as an argument.
+    //console.log('magic methods: ', kevin.__proto__) <-- this logs all the methods available on an *instance*. FYI: if you call this on the model you won't see the magic methods.
     const doppio = await Pet.create({
-        name: "Doppio",
+        name: 'Doppio',
         age: 2,
-        //userId: 1
+        userId: 1
     })
-    doppio.setUser(kevin)
+    await Pet.create({
+        name: 'Bella',
+        age: 10
+    })
+    //doppio.setUser(kevin) <-- Instead of passing userId:1 like we did above, we can also use the *magic method* 
     console.log('pet magic methods: ', doppio.__proto__)
+
+    // Also we didn't have time to do this in the demo but we did go over it in the slide deck. 
+    // If you want to query a table you can use methods like findAll to retrieve all rows, findOne to retrieve one row, etc. 
+    // Check out the Sequelize docs to see what other methods you can use: https://sequelize.org/master/manual/model-querying-basics.html
+    User.findAll() // will retrieve all the rows from the users table in an array of objects. 
+    Pet.findOne({ // will retrieve one row from pets table where name = Doppio
+        where: {name: 'Doppio'}
+    })
+
 }
 connect()
